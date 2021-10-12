a7b9d82928f1bfbf917ae9f572d0899d5118e967
MrChampion91 <vav206g@gmail.com>
2021-10-12 11:18:52 +0300

    add textures on box

--- box_with_texture.py
+++ box_with_texture.py
@@ -1,1 +1,155 @@
-None
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy
+import pyrr
+from PIL import Image
+
+def main():
+
+    # initialize glfw
+    if not glfw.init():
+        return
+
+    window = glfw.create_window(800, 600, "My OpenGL window", None, None)
+
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    #        positions        colors            text coord
+    cube = [-0.5, -0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+             0.5, -0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+             0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, -0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+            0.5, 0.5, -0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+            -0.5, 0.5, -0.5, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+            0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+            0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+            0.5, -0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+            -0.5, 0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+            -0.5, -0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+            -0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+            -0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+            0.5, -0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+            0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+            -0.5, -0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0,
+
+            0.5, 0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 0.0,
+            -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0,
+            -0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0,
+            0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 1.0]
+
+    cube = numpy.array(cube, dtype = numpy.float32)
+
+    indices = [0, 1, 2, 2,
+               3, 0, 4, 5,
+               6, 6, 7, 4,
+               8, 9, 10, 10,
+               11, 8, 12, 13,
+               14, 14, 15, 12,
+               16, 17, 18, 18,
+               19, 16, 20, 21,
+               22, 22, 23, 20]
+
+    indices = numpy.array(indices, dtype= numpy.uint32)
+
+    vertex_shader = """
+    #version 330
+    in layout(location = 0) vec3 position;
+    in layout(location = 1) vec3 color;
+    in layout(location = 2) vec2 textureCoords;
+    uniform mat4 transform;
+    out vec3 newColor;
+    out vec2 newTexture;
+    void main()
+    {
+        gl_Position = transform * vec4(position, 1.0f);
+        newColor = color;
+        newTexture = textureCoords;
+    }
+    """
+
+    fragment_shader = """
+    #version 330
+    in vec3 newColor;
+    in vec2 newTexture;
+    out vec4 outColor;
+    uniform sampler2D samplerTexture;
+    void main()
+    {
+        outColor = texture(samplerTexture, newTexture) ;
+    }
+    """
+    #outColor = texture(samplerTexture, newTexture) * vec4(newColor, 1.0f);
+
+    shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
+                                              OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
+
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, 4 * len(cube), cube, GL_STATIC_DRAW)
+
+    EBO = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * len(indices), indices, GL_STATIC_DRAW)
+
+    position = glGetAttribLocation(shader, "position")
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(position)
+
+    #color = glGetAttribLocation(shader, "color")
+    #glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
+    #glEnableVertexAttribArray(color)
+
+    texture_coords = glGetAttribLocation(shader, "textureCoords")
+    glVertexAttribPointer(texture_coords, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))
+    glEnableVertexAttribArray(texture_coords)
+
+
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+    image = Image.open("textures/wood25_512.jpg")
+    img_data = numpy.array(list(image.getdata()), numpy.uint8)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.width, image.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+
+
+    glUseProgram(shader)
+
+    glClearColor(0.2, 0.3, 0.2, 1.0)
+    glEnable(GL_DEPTH_TEST)
+    #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        rot_x = pyrr.Matrix44.from_x_rotation(0.5 * glfw.get_time())
+        rot_y = pyrr.Matrix44.from_y_rotation(0.8 * glfw.get_time())
+
+        transformLoc = glGetUniformLocation(shader, "transform")
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, rot_x * rot_y)
+
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, None)
+
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
